cdda29f62ea92c67090798bc96e20cbb57eca308
robatsaz145 <89588786+robatsaz145@users.noreply.github.com>
2022-07-02 15:37:44 +0430

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://airdrop_22eed:SBMCD3ylHmWTxFcl@cluster0.sidoc.mongodb.net/?retryWrites=true&w=majority',// Database Link Of MongoDB
+
+apikey:'xxxx',// Coinbase API Key
+
+secretkey:'xxxxxx',// Coinbase 2nd Key
+
+bot_token:'5517870732:AAEqH7vO5VU82lUCJq_YUO_eKizodasEvrw',// Bot Token
+
+bot_name:'Gdsgghbot',// Bot Username Without @
+
+bot_admin:5159732126,// Admin Telegram Id
+
+channelsList : ['@TXTPayouts'],// Channels List
+
+channelscheck : ['@fjfndndnfbt'],// Check Channels List
+
+reffer_bonus:0.01,// Refferal Bonus Amount
+
+min_wd:0.001,// Min Withdrawal Amount
+
+daily_bonus:1000,// Daily Bonus Amount
+
+currency:'DOGE', // Bot Currency
+
+payment_channel:'@fjfndndnfbt'// Payment Channel Username
+
+}
